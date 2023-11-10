34c80d65836563703ea1a17c102dfb677c4a607f
anna2610-an <ka244712610@gmail.com>
2023-11-10 17:14:28 +0700

    sửa panel chi tiết từng thành phố

--- donace-fe/components/explore/cities/cantho.tsx
+++ donace-fe/components/explore/cities/cantho.tsx
@@ -6,12 +6,28 @@ import React from "react";
 export default function CanTho() {
   return (
     <div className="panel ">
-      <Image
-        alt="Panel"
-        className="object-cover"
-        width={1000}
-        src="https://file1.dangcongsan.vn/data/0/images/2021/04/27/vuongle/ttxxvnktvn.jpg"
-      />
+      <div className=" h-[400px] bg-gradient-to-r from-violet-300 to-rose-300 ">
+        <div className="head w-[800px] m-auto h-[100%] grid grid-cols-2  overflow-hidden">
+          <div className="text z-50 grid grid-cols-1 content-center">
+            <h1 className="text-3xl font-medium text-[rgba(19,21,23,0.36)] dark:text-[hsla(0,0%,100%,.5)] mt-0 mb-4">
+              Những sự kiện ở
+            </h1>
+            <h1
+              id="title"
+              className="text-4xl font-semibold text-[rgb(19,21,23)] dark:text-[#fff] mb-0 mt-0"
+            >
+              Cần Thơ
+            </h1>
+          </div>
+          <div className="img grid-cols-2">
+            <img
+              src="https://res.cloudinary.com/deupkdvle/image/upload/v1699610509/z4867580777094_71d800d720a55b27bb4f97f342cfc506-removebg-preview_kcukkn.png"
+              alt=""
+              className=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- donace-fe/components/explore/cities/hue.tsx
+++ donace-fe/components/explore/cities/hue.tsx
@@ -6,12 +6,28 @@ import React from "react";
 export default function Hue() {
   return (
     <div className="panel ">
-      <Image
-        alt="Panel"
-        className="object-cover"
-        width={1000}
-        src="https://file1.dangcongsan.vn/data/0/images/2021/04/27/vuongle/ttxxvnktvn.jpg"
-      />
+      <div className=" h-[400px] bg-gradient-to-r from-rose-300 to-amber-300 ">
+        <div className="head w-[800px] m-auto h-[100%] grid grid-cols-2  overflow-hidden">
+          <div className="text z-50 grid grid-cols-1 content-center">
+            <h1 className="text-3xl font-medium text-[rgba(19,21,23,0.36)] dark:text-[hsla(0,0%,100%,.5)] mt-0 mb-4">
+              Những sự kiện ở
+            </h1>
+            <h1
+              id="title"
+              className="text-4xl font-semibold text-[rgb(19,21,23)] dark:text-[#fff] mb-0 mt-0"
+            >
+              Huế
+            </h1>
+          </div>
+          <div className="img grid-cols-2">
+            <img
+              src="https://res.cloudinary.com/deupkdvle/image/upload/v1699611051/hue-vietnam-travel-itinerary-02-removebg-preview_bdk81t.png"
+              alt=""
+              className=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
