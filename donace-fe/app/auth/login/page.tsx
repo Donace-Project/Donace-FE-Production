f13a03e39412f2f096484035096d5532d15985f8
GiaBao <nguyenhiengiabao12@gmail.com>
2023-11-13 16:56:14 +0700

    - Fixing UI in error notification - Remove navbar in Login & Register page - Fixing UI in Login & Register form

--- donace-fe/app/auth/login/page.tsx
+++ donace-fe/app/auth/login/page.tsx
@@ -2,12 +2,9 @@
 import "@/styles/globals.css";
 import React from 'react';
 import SignIn from "@/components/login/signIn";
-import NavbarLanding from "@/components/navbarLandingPage";
-
 export default function Page() {
     return (
         <div className="page-wrapper gradient-background">
-            <NavbarLanding/>
             <div className="main">
                 <SignIn />
             </div>
--- donace-fe/components/login/signIn.tsx
+++ donace-fe/components/login/signIn.tsx
@@ -39,75 +39,75 @@ export default function SignIn() {
       router.push("/home");
     } else {
       // Xử lý hiển thị lỗi
-      setError("Sai tài khoản hoặc mật khẩu");
+      setError("Sai tài khoản hoặc mật khẩu.");
     }
   };
 
+
   return (
     <div className="onboarding-page">
-      <Card className="opacity-[1] relative flex-1 p-6 bg-[rgba(255,255,255,0.8)] backdrop-blur-lg max-w-sm rounded-2xl h-fit">
-        <div className="relative h-[480px]">
-          <div className="animated-container absolute top-0 w-full">
-            <CardHeader className="opacity-[1] transform-none">
-              <div className="icon relative w-16 h-16 flex items-center justify-center bg-[rgba(19,21,23,0.04)] rounded-full">
-                <DoorOpen className="w-8 h-8 text-[#939597] block align-middle" />
-                <ArrowLeft className="top-6 right-2.5 absolute text-[#939597] block w-4 h-4 align-middle" />
-              </div>
-            </CardHeader>
-            <CardBody className="opacity-[1] transform-none">
-              <div className="gap-2 pt-1 mb-4 flex flex-col">
-                <h1 className="font-semibold text-2xl mb-0 mt-0 leading-5">
-                  Chào mừng đến Donace
-                </h1>
-                <div className="text-secondary pt-0.5 pb-1 text-sm text-black-more-blur-light-theme">
-                  Vui lòng đăng ký hoặc đăng nhập bên dưới.
-                </div>
-              </div>
-              <div>
-                <Input
-                  value={email}
-                  type="email"
-                  label="Email"
-                  isInvalid={isInvalidEmail}
-                  color={isInvalidEmail ? "danger" : "success"}
-                  errorMessage={isInvalidEmail && "Vui lòng điền đúng email"}
-                  onValueChange={setEmail}
-                  isClearable
-                  placeholder="Email của bạn"
-                  className="text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0"
-                />
-                <Input
-                  label="Mật khẩu"
-                  value={password}
-                  onValueChange={setPassword}
-                  labelPlacement={"inside"}
-                  placeholder="Mật khẩu của bạn"
-                  className="pt-2 text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0 mb-3 transparentInput"
-                  type={isVisible ? "text" : "password"}
-                />
-                <div className="mb-5">
-                  {error && <div className="text-red-500">
-                    <div className="label break-words">{error}</div>
-                  </div>}
-                </div>
-                <Button
-                  onClick={onSubmit}
-                  type="button"
-                  className="mb-12 text-[#fff] bg-[#333537] border-[#333537] border border-solid w-full cursor-pointer transition-all duration-300 ease-in-out font-medium rounded-lg relative whitespace-nowrap justify-center outline-none max-w-full text-base p-[0.625rem_0.875rem] h-[calc(2.25rem+2*1px)] flex items-center m-0 leading-6"
-                >
-                  <div className="label">Đăng nhập</div>
-                </Button>
-              </div>
-            </CardBody>
-            <Divider />
-            <CardFooter className="-mb-2 flex justify-center items-center">
-              <div className="">Bạn không có tài khoản?</div>
-              <div>&nbsp;</div>
-              <Link href="/auth/register" underline="hover">
-                Đăng ký ngay
-              </Link>
-            </CardFooter>
-          </div>
+      <Card className="onboarding-card opacity-[1] relative flex-1 p-6 bg-[rgba(255,255,255,0.8)] backdrop-blur-lg rounded-3xl overflow-visible max-w-sm w-full">
+        <div className="relative">
+          <CardHeader className="opacity-[1] transform-none">
+            <div className="icon relative w-16 h-16 flex items-center justify-center bg-[rgba(19,21,23,0.04)] rounded-full">
+              <DoorOpen className="w-8 h-8 text-[#939597] block align-middle" />
+              <ArrowLeft className="top-6 right-2.5 absolute text-[#939597] block w-4 h-4 align-middle" />
+            </div>
+          </CardHeader>
+          <CardBody>
+            <h1 className="font-semibold text-2xl mb-0 mt-0 leading-5">
+              Chào mừng đến Donace
+            </h1>
+            <div className="text-secondary pt-4 pb-5 text-sm text-black-more-blur-light-theme">
+              Vui lòng đăng ký hoặc đăng nhập bên dưới.
+            </div>
+            <Input
+              value={email}
+              type="email"
+              label="Email"
+              isInvalid={isInvalidEmail}
+              color={isInvalidEmail ? "danger" : "success"}
+              errorMessage={isInvalidEmail && "Vui lòng điền đúng email."}
+              onValueChange={setEmail}
+              isClearable
+              placeholder="Email của bạn"
+              className="text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0"
+              classNames={{
+                errorMessage: [
+                  "text-base"
+                ]
+              }}
+            />
+            <Input
+              label="Mật khẩu"
+              value={password}
+              onValueChange={setPassword}
+              labelPlacement={"inside"}
+              placeholder="Mật khẩu của bạn"
+              className="pt-2 text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0 mb-4 transparentInput"
+              type={isVisible ? "text" : "password"}
+            />
+            <div className="mb-3 ml-1">
+              {error && <div className="text-[#f3236a]">
+                <div className="label break-words">{error}</div>
+              </div>}
+            </div>
+            <Button
+              onClick={onSubmit}
+              type="button"
+              className="mb-12 text-[#fff] bg-[#333537] border-[#333537] border border-solid w-full cursor-pointer transition-all duration-300 ease-in-out font-medium rounded-lg relative whitespace-nowrap justify-center outline-none max-w-full text-base p-[0.625rem_0.875rem] h-[calc(2.25rem+2*1px)] flex items-center m-0 leading-6"
+            >
+              <div className="label">Đăng nhập</div>
+            </Button>
+          </CardBody>
+          <Divider />
+          <CardFooter className="-mb-2 flex justify-center items-center">
+            <div className="">Bạn không có tài khoản?</div>
+            <div>&nbsp;</div>
+            <Link href="/auth/register" underline="hover">
+              Đăng ký ngay
+            </Link>
+          </CardFooter>
         </div>
       </Card>
     </div>
