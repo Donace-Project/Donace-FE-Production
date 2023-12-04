dc06d73ad6e31f6178d2d1fb63f6ce728c2982c6
trandat1114 <cctalk1114@gmail.com>
2023-12-04 23:29:40 +0700

    Remove unnecessary files and add new dependencies feat: generator QR code

---
+++ donace-fe/app/qrcode-demo/page.tsx
@@ -0,0 +1,65 @@
+"use client"
+// pages/index.js
+import React from 'react';
+import QRCodeGenerator from '@/components/QR/QRGenerator';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Divider } from "@nextui-org/react";
+
+const QRCodeDemoPage = () => {
+
+  var informations = {
+    "name": "Trần Phú Đạt",
+    "email": "jackandy249@gmail.com",
+    "ticket": "VIP"
+  }
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  let qrCodeValue = "your-qr-code-value"; // Replace with your actual QR code value
+
+
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+
+      <Button onPress={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement='center' size='xs' >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody className='p-8'>
+                <div className='w-full h-full'>
+                  <div className='w-min h-min p-8 rounded-lg bg-transparent backdrop-blur-sm border-dashed border-black border-2 m-auto'>
+                    <QRCodeGenerator value={qrCodeValue} />
+                  </div>
+                  <Divider orientation="horizontal" className='my-4' />
+                </div>
+                <div>
+                  <p className='text-2xl'>
+                    Tên sự kiện
+                  </p>
+                </div>
+                <div className='grid grid-cols-2 gap-2 justify-between'>
+                  <div className='grid grid-rows-2'>
+                    <p>Tên</p>
+                    <p className='truncate'>{informations.name}</p>
+                  </div>
+                  <div className='grid grid-rows-2'>
+                    <p>Email</p>
+                    <p className='truncate'>{informations.email}</p>
+                  </div>
+                  <div className='grid grid-rows-2'>
+                    <p>Hạng vé</p>
+                    <p className='truncate'>{informations.ticket}</p>
+                  </div>
+                </div>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default QRCodeDemoPage;
