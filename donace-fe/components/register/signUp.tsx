71e4fbdede939aa241a7e44861b7d3c6f423013b
tungnhps17361 <tungnhps17361@fpt.edu.vn>
2023-12-06 02:24:28 +0700

    chore: xử lý loading cho màn đăng ký

--- donace-fe/components/register/signUp.tsx
+++ donace-fe/components/register/signUp.tsx
@@ -10,6 +10,7 @@ import { Divider } from "@nextui-org/divider";
 import { fetchWrapper } from "../../helpers/fetch-wrapper";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { Spinner } from "@nextui-org/react";
 
 export default function SignUp() {
   const [isVisible, setIsVisible] = React.useState(false);
@@ -18,6 +19,8 @@ export default function SignUp() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState("");
+
+  const [isLoading, SetIsLoading] = React.useState(false);
 
   const validateEmail = (value: string) =>
     value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
@@ -29,6 +32,18 @@ export default function SignUp() {
   }, [email]);
 
   const submit = async () => {
+    if (email === "" || password === "") {
+      setError("Vui lòng nhập đẩy đủ thông tin");
+      return;
+    }
+
+    debugger;
+    if(isInvalidEmail) {
+      setError("Vui lòng nhập email hợp lệ");
+      return;
+    }
+
+    SetIsLoading(true);
     var resultRegister = await fetchWrapper.post(
       "/api/Authentication/register",
       {
@@ -38,7 +53,13 @@ export default function SignUp() {
     );
 
     if (resultRegister?.result?.succeeded === false) {
-      setError(resultRegister?.result?.errors[0]?.description);
+      if (resultRegister?.result?.errors[0]?.code === "DuplicateUserName") {
+        setError("Tài khoản đã tổn tại, Vui lòng dùng Email khác");
+      } else {
+        setError(resultRegister?.result?.errors[0]?.description);
+      }
+
+      SetIsLoading(false);
       return;
     }
 
@@ -52,9 +73,9 @@ export default function SignUp() {
       router.push("/home");
     } else {
       setError("Đăng ký thành công, nhưng tạm thời không đăng nhập được");
+      SetIsLoading(false);
     }
   };
-
 
   return (
     <div className="onboarding-page">
@@ -88,9 +109,7 @@ export default function SignUp() {
                 placeholder="Email của bạn"
                 className="text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0"
                 classNames={{
-                  errorMessage: [
-                    "text-base"
-                  ]
+                  errorMessage: ["text-base"],
                 }}
               />
               <Input
@@ -103,19 +122,34 @@ export default function SignUp() {
                 className="pt-2 text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0 mb-4"
                 type={isVisible ? "text" : "password"}
               />
-            <div className="mb-3 ml-1">
-              {error && (
-                <div className="text-[#f3236a]">
-                  <div className="label break-words">{error}</div>
-                </div>
-              )}
-            </div>
+              <div className="mb-3 ml-1">
+                {error && (
+                  <div className="text-[#f3236a]">
+                    <div className="label break-words">{error}</div>
+                  </div>
+                )}
+              </div>
               <Button
                 type="button"
                 onClick={submit}
                 className="mb-12 text-[#fff] bg-[#333537] border-[#333537] border border-solid w-full cursor-pointer transition-all duration-300 ease-in-out font-medium rounded-lg relative whitespace-nowrap justify-center outline-none max-w-full text-base p-[0.625rem_0.875rem] h-[calc(2.25rem+2*1px)] flex items-center m-0 leading-6"
               >
-                <div className="label">Đăng ký</div>
+                <div className="label">
+                  {isLoading ? (
+                    <>
+                      <Spinner
+                        size="sm"
+                        color="success"
+                        className="translate-y-0.5 mr-2"
+                      />
+                      <span className="label">Đang đăng ký..</span>
+                    </>
+                  ) : (
+                    <>
+                      <div className="label">Đăng ký</div>
+                    </>
+                  )}
+                </div>
               </Button>
             </div>
           </CardBody>
@@ -130,76 +164,5 @@ export default function SignUp() {
         </div>
       </Card>
     </div>
-    // <div className="onboarding-page">
-    //   <Card className="opacity-[1] relative flex-1 p-6 bg-[rgba(255,255,255,0.8)] backdrop-blur-lg max-w-sm rounded-2xl h-fit">
-    //     <div className="relative h-[480px]">
-    //       <div className="animated-container absolute top-0 w-full">
-    //         <CardHeader className="opacity-[1] transform-none">
-    //           <div className="icon relative w-16 h-16 flex items-center justify-center bg-[rgba(19,21,23,0.04)] rounded-full">
-    //             <UserPlus className="w-8 h-8 text-[#939597] block align-middle ml-1" />
-    //           </div>
-    //         </CardHeader>
-    //         <CardBody className="opacity-[1] transform-none">
-    //           <div className="gap-2 pt-1 mb-4 flex flex-col">
-    //             <h1 className="font-semibold text-2xl mb-0 mt-0 leading-5">
-    //               Đăng ký
-    //             </h1>
-    //             <div className="text-secondary pt-0.5 pb-1 text-sm text-black-more-blur-light-theme">
-    //               Tạo tài khoản mới để sử dụng dịch vụ.
-    //             </div>
-    //           </div>
-    //           <div>
-    //             <Input
-    //               value={email}
-    //               type="email"
-    //               label="Email"
-    //               isInvalid={isInvalidEmail}
-    //               color={isInvalidEmail ? "danger" : "success"}
-    //               errorMessage={isInvalidEmail && "Email không hợp lệ"}
-    //               onValueChange={setEmail}
-    //               isRequired
-    //               isClearable
-    //               placeholder="Email của bạn"
-    //               className="text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m-0"
-    //             />
-    //             <Input
-    //               label="Mật khẩu"
-    //               value={password}
-    //               onValueChange={setPassword}
-    //               isRequired
-    //               labelPlacement={"inside"}
-    //               placeholder="Password của bạn"
-    //               className="pt-2 text-base h-auto transition-all duration-300 leading-4 rounded-lg w-full m- mb-6"
-    //               type={isVisible ? "text" : "password"}
-    //             />
-    //             <Button
-    //               type="button"
-    //               onClick={submit}
-    //               className="mb-12 text-[#fff] bg-[#333537] border-[#333537] border border-solid w-full cursor-pointer transition-all duration-300 ease-in-out font-medium rounded-lg relative whitespace-nowrap justify-center outline-none max-w-full text-base p-[0.625rem_0.875rem] h-[calc(2.25rem+2*1px)] flex items-center m-0 leading-6"
-    //             >
-    //               <div className="label">Đăng ký</div>
-    //             </Button>
-    //           </div>
-
-    //           <div className="mt-5">
-    //             {error && (
-    //               <div className="text-red-500">
-    //                 <div className="label break-words">{error}</div>
-    //               </div>
-    //             )}
-    //           </div>
-    //         </CardBody>
-    //         <Divider />
-    //         <CardFooter className="-mb-2 flex justify-center items-center">
-    //           <div className="">Đã có tài khoản?</div>
-    //           <div>&nbsp;</div>
-    //           <Link href="/auth/login" underline="hover">
-    //             Đăng nhập
-    //           </Link>
-    //         </CardFooter>
-    //       </div>
-    //     </div>
-    //   </Card>
-    // </div>
   );
 }
