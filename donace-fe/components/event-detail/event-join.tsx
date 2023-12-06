fca9052f0b41be8bd889d12dcc35509543bffd40
GiaBao <nguyenhiengiabao12@gmail.com>
2023-12-07 00:53:42 +0700

    style (UI): add screen setting payment in Event Manage

--- donace-fe/components/event-detail/event-join.tsx
+++ donace-fe/components/event-detail/event-join.tsx
@@ -129,6 +129,7 @@ export default function EventUserJoin(props: any) {
                                 Khách
                             </Link>
                             <Link
+                                href={`/events/manage/${eventDetail?.id}/payment`}
                                 className="text-black-blur-light-theme dark:text-[hsla(0,0%,100%,.5)] border-b-2 border-solid border-transparent whitespace-nowrap inline-block pb-2 transition-all duration-300 ease-in-out cursor-pointer"
                                 underline="none"
                             >
