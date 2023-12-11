3a4d36f6d81c4e21b92b996af4f33b40be137c9b
trandat1114 <cctalk1114@gmail.com>
2023-12-12 05:43:13 +0700

    Refactor JoinEvent component to handle QR code generation

--- donace-fe/components/home/home.tsx
+++ donace-fe/components/home/home.tsx
@@ -6,7 +6,7 @@ import { ArrowRight, CalendarClock, Frown, MapPin, Plus, Radio, ScanLine, Users2
 import "@/styles/globals.css";
 import { fetchWrapper } from "@/helpers/fetch-wrapper";
 import { Image } from "@nextui-org/image";
-import { Tabs, Tab, Divider } from "@nextui-org/react";
+import { Tabs, Tab, Divider, useDisclosure } from "@nextui-org/react";
 import { Skeleton } from "@nextui-org/skeleton";
 interface DateTimeInfo {
   year: string;
@@ -54,6 +54,8 @@ export type Item = {
   totalGuest: number;
   calendarId: string;
   isLive: boolean;
+  isHost: boolean;
+  isOnline: boolean;
 }
 
 const CovertDate = (date: string) => {
@@ -108,6 +110,7 @@ export default function HomeEvents() {
       try {
         const futureEventsData = await fetchWrapper.get(`api/Event?IsNew=${dateTimeTrue}`);
         setFutureEvents(futureEventsData.items);
+        console.log(futureEventsData.items);
         setLoading(false);
 
         const pastEventsData = await fetchWrapper.get(`api/Event?IsNew=${dateTimeFalse}`);
@@ -124,6 +127,27 @@ export default function HomeEvents() {
 
   const gio = thoiGian.getHours();
   const buoi = gio >= 12 ? "PM" : "AM";
+
+
+  //QR generator
+  const modalViewTicket = useDisclosure();
+  const [ticketIdForQr, setTicketIdForQr] = useState<string>("");
+  const handleQrGenerator = async () => {
+    if (ticketIdForQr == "") {
+      let ticketId = await fetchWrapper.get("/api/UserTickets/get-ticket")
+      if (ticketId != null) {
+        setTicketIdForQr(ticketId)
+      }
+      else {
+        console.log("some bug")
+      }
+    }
+  }
+
+  const openModalGenQr = () => {
+    handleQrGenerator();
+    modalViewTicket.onOpen();
+  }
 
   return (
     <div className="page-content">
@@ -182,16 +206,16 @@ export default function HomeEvents() {
                             </div>
                             <div className="w-full">
                               <div className="card-wrapper">
-                                <div className="card-wrapper content-card cursor-pointer transition-all duration-300 ease-in-out rounded-xl bg-[#f3f4f5] dark:bg-[rgba(255,255,255,0.04)] border border-solid border-[#f3f4f5] dark:border-[rgba(255,255,255,0.04)]">
+                                <div className="card-wrapper content-card transition-all duration-300 ease-in-out rounded-xl bg-[#f3f4f5] dark:bg-[rgba(255,255,255,0.04)] border border-solid border-[#f3f4f5] dark:border-[rgba(255,255,255,0.04)]">
                                   <div className="event-content gap-3 flex flex-col">
                                     <div className="info-and-cover flex-col md:flex-row-reverse gap-4 flex">
-                                      <div className="block">
+                                      <Link href={`${event.isHost ? `/events/manage/${event.id}` : `/user/join-event/${event.id}`}`} className="block">
                                         <div className="w-full h-full md:w-40 md:h-40">
                                           <div className="img-aspect-ratio w-full h-full rounded-lg">
                                             <Image className="w-full h-full" alt="you are invited" src={event.cover} />
                                           </div>
                                         </div>
-                                      </div>
+                                      </Link>
                                       <div className="info gap-2 min-w-0 flex-1 flex flex-col">
                                         <div className="event-time gap-2 flex items-center">
                                           {event.isLive ? (
@@ -243,20 +267,38 @@ export default function HomeEvents() {
                                       </div>
                                     </div>
                                     <div className="event-bottom-bar flex justify-between items-center">
-                                      <div className="gap-2 flex flex-col md:flex-row items-center justify-between w-full">
-                                        <Button
-                                          as={Link}
-                                          href={`/user/join-event/${event.id}`}
-                                          className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer"
-                                        >
-                                          <div className="label">Check In</div>
-                                          <ScanLine className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle mt-0.5" />
-                                        </Button>
-                                        <Button as={Link} href={`/events/manage/${event.id}`} className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer">
-                                          <div className="label">Quản lý sự kiện</div>
-                                          <ArrowRight className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle" />
-                                        </Button>
-                                      </div>
+                                      {
+                                        event.isHost ?
+                                          <div className="gap-2 flex flex-col md:flex-row items-center justify-between w-full">
+                                            <Button
+                                              as={Link}
+                                              href={`/user/join-event/${event.id}`}
+                                              className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer"
+                                            >
+                                              <div className="label">Check In</div>
+                                              <ScanLine className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle mt-0.5" />
+                                            </Button>
+                                            <Button as={Link} href={`/events/manage/${event.id}`} className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer">
+                                              <div className="label">Quản lý sự kiện</div>
+                                              <ArrowRight className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle" />
+                                            </Button>
+                                          </div>
+                                          :
+                                          <div className="gap-2 flex flex-col md:flex-row items-center justify-between w-full">
+                                            <Button
+                                              onClick={() => { openModalGenQr(); }}
+                                              className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer"
+                                            >
+                                              <div className="label">Xem mã QR</div>
+                                              <ScanLine className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle mt-0.5" />
+                                            </Button>
+                                            <Button as={Link} href={`/user/join-event/${event.id}`} className="text-black-more-blur-light-theme dark:text-[rgba(255,255,255,0.64)] bg-[rgba(19,21,23,0.04)] dark:bg-[rgba(255,255,255,0.08)] border-transparent border border-solid transition-all duration-300 ease-in-out donace-button flex items-center cursor-pointer">
+                                              <div className="label">Xem sự kiện</div>
+                                              <ArrowRight className="mr-1.5 stroke-2 w-3.5 h-3.5 flex-shrink-0 block align-middle" />
+                                            </Button>
+                                          </div>
+                                      }
+
                                     </div>
                                   </div>
                                 </div>
