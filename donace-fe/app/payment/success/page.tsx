47b610f2eb218a17430bb6af6e31d300c0970319
trandat1114 <cctalk1114@gmail.com>
2023-12-18 04:38:42 +0700

    Remove unused variable and reformat code

--- donace-fe/app/payment/success/page.tsx
+++ donace-fe/app/payment/success/page.tsx
@@ -5,11 +5,9 @@ import { useParams } from 'next/navigation';
 
 
 export default function Page() {
-    const { vnp_OrderType } = useParams() as { vnp_OrderType: string };
     return (
-        <>
-            <h1>{vnp_OrderType}</h1>
-            <ReturnPaymentURL />
-        </>
+
+        <ReturnPaymentURL />
+
     )
 }
